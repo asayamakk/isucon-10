cd578f2ea0b10a26d74dcfc87ede60ae3b5db057
asayamakk <kodai.asayama@gmail.com>
2020-10-04 16:36:46 +0900

    update schema

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    calculated_conditions INTEGER
 );
 
 CREATE TABLE isuumo.chair
@@ -34,5 +35,11 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    calculated_conditions INTEGER
 );
+
+create index idx_chair_popularity on chair (popularity desc, id asc);
+create index idx_estate_popularity on estate (popularity desc, id asc);
+create index idx_chair_conds on chair (calculated_conditions, features) ;
+create index idx_estate_conds on estate (calculated_conditions, features);
